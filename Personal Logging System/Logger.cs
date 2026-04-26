34ddb348cc2c71e8939ae8e43250156ef827fc3f
hheydarian <hamdhydryan12@gmail.com>
2026-04-26 16:39:02 +0330

    Complate

--- Personal Logging System/Logger.cs
+++ Personal Logging System/Logger.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
 namespace Personal_Logging_System
 {
     public class Logger
     {
+        private readonly string _logFilePath = "app.log";
 
+        public async Task LogError(string message)
+        {
+            var callerInfo = GetCallerInfo();
+
+            string logEntry = $"[{DateTime.Now:yyyy:MM:dd HH:mm:ss}] [ERROR] {callerInfo} - {message}\n";
+
+            await File.AppendAllTextAsync(_logFilePath, logEntry, Encoding.UTF8);
+        }
+        private string GetCallerInfo()
+        {
+            var stackTrace = new System.Diagnostics.StackTrace(skipFrames: 1, fNeedFileInfo: true);
+            var frame = stackTrace.GetFrame(0);
+            var method = frame.GetMethod();
+            var location = frame.GetFileName();
+
+            string className = method.DeclaringType?.Name ?? "Unknown";
+            string methodName = method.Name;
+            string lineNumber = frame.GetFileLineNumber().ToString();
+
+            return $"{className}.{methodName} (Line: {lineNumber})";
+        }
     }
 }
--- Personal Logging System/Program.cs
+++ Personal Logging System/Program.cs
@@ -2,9 +2,24 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static readonly Logger _logger = new();
+        static async Task Main(string[] args)
         {
-            
+            Console.WriteLine("Starting Aplication ...");
+
+            try
+            {
+                ProcessData();
+            }
+            catch(Exception ex) 
+            {
+                await _logger.LogError(ex.Message);
+                Console.WriteLine("Error logged. Check app.log");
+            }
+        }
+        static void ProcessData()
+        {
+            throw new Exception("Something went wrong in ProcessData!");
         }
     }
 }
