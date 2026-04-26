56fb29138f0694b1dac7add384efdc26fdaeffbb
hheydarian <hamdhydryan12@gmail.com>
2026-04-26 16:13:12 +0330

    Add project files.

---
+++ Personal Logging System/Logger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Personal_Logging_System
+{
+    public class Logger
+    {
+
+    }
+}
---
+++ Personal Logging System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Personal_Logging_System
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
